45ddb5cf97da56e7e085e058fe18896d095c01ea
Gmishra2000 <mantumisara485@gmail.com>
2021-06-14 18:14:15 +0530

    basics of pagination

--- src/component/MoviesPage.jsx
+++ src/component/MoviesPage.jsx
@@ -4,12 +4,14 @@ import { getMovies } from '../temp/MovieService'
 export default class MoviesPage extends Component {
     state = {
         movies: getMovies(),
-        currSearchText: ""
-        
+        currSearchText: "",
+        limit: 4,
+        currentPage:1 
+
     }
     deleteEntry = (id) => {
         let filtereMovies = this.state.movies
-            .filter( (movieObj) => {
+            .filter((movieObj) => {
                 return movieObj._id !== id;
             })
         this.setState({
@@ -19,7 +21,7 @@ export default class MoviesPage extends Component {
 
     setCurrentText = (e) => {
         let task = e.target.value;
-        
+
         this.setState({
             currSearchText: task
 
@@ -32,7 +34,7 @@ export default class MoviesPage extends Component {
         let { movies } = this.state;
         if (className == "fas fa-sort-up") {
             sortedMovies = movies.sort((movieObjA, movieObjB) => {
-                return movieObjA.dailyRentalRate - movieObjB.dailyRentalRate;
+                return movieObjA.dailyRentalRate - movieObjB.dailyRentalRate; // used sort function to sort on the basis of rating and stocks
             })
         } else {
             sortedMovies = movies.sort((movieObjA, movieObjB) => {
@@ -40,7 +42,16 @@ export default class MoviesPage extends Component {
             })
         }
         this.setState({
-            movies : sortedMovies
+            movies: sortedMovies
+        })
+    }
+
+    // alow to change the no of elements in page
+    changelimit = (e) => {
+        console.log("hello");
+        let currLimit = e.target.value;
+        this.setState({
+            limit:currLimit
         })
     }
 
@@ -50,7 +61,7 @@ export default class MoviesPage extends Component {
         let { movies } = this.state;
         if (className == "fas fa-sort-up") {
             sortedMovies = movies.sort((movieObjA, movieObjB) => {
-                return movieObjA.numberInStock - movieObjB.numberInStock;
+                return movieObjA.numberInStock - movieObjB.numberInStock; // https://www.w3schools.com/js/js_array_sort.asp
             })
         } else {
             sortedMovies = movies.sort((movieObjA, movieObjB) => {
@@ -63,32 +74,38 @@ export default class MoviesPage extends Component {
     }
     render() {
         // console.log(this.state.movies);
-        let { movies, currSearchText } = this.state;
+        let { movies, currSearchText,limit,currentPage} = this.state;
         let filteredArr = movies.filter((movieObj) => {
             let title = movieObj.title.trim().toLowerCase();
             return title.includes(currSearchText.trim().toLowerCase());
         })
-        
+
         if (currSearchText === "") {
             filteredArr = this.state.movies;
         }
+        // console.log(filteredArr);
+        // si -> (pageNumber - 1) * limit;
+        // ei -> si + limit
         // console.log(currSearchText);
+        let si = (currentPage - 1) * limit;
+        let eidx = si + limit;
+        filteredArr = filteredArr.slice(si, eidx);
         return (
             <div className="row">
                 <div className="col-3">
                     hello
                 </div>
                 <div className="col-9">
-                    <input type="search" value={currSearchText} onChange= {this.setCurrentText} />
+                    <input type="search" value={currSearchText} onChange={this.setCurrentText} />
                     <table className="table">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Title</th>
                                 <th scope="col">Genre</th>
-                                
+
                                 <th scope="col">
-                                    <i className="fas fa-sort-up" onClick={this.sortByStock}></i>    
+                                    <i className="fas fa-sort-up" onClick={this.sortByStock}></i>
                                     Stock
                                     <i className="fas fa-sort-down" onClick={this.sortByStock}></i>
                                 </th>
@@ -96,33 +113,52 @@ export default class MoviesPage extends Component {
                                     Rate
                                     <i className="fas fa-sort-down" onClick={this.sortByRatings}></i>
                                 </th>
-                            
+
                             </tr>
-                            
+
                         </thead>
                         <tbody>
-                            {filteredArr.map((movieObj) =>{
+                            {filteredArr.map((movieObj) => {
 
                                 return (<tr scope="row" key={movieObj._id} >
                                     <td></td>
-                                <td>{movieObj.title}   </td>
-                                
-                                <td>{movieObj.genre.name}</td>
-                                <td>{movieObj.numberInStock}</td>
-                                <td>{movieObj.dailyRentalRate}</td>
-                                <td><button type="button" className="btn btn-danger" onClick={() => { this.deleteEntry(movieObj._id); }}>Delete</button></td>
-                                
-                            </tr>
+                                    <td>{movieObj.title}   </td>
 
-                                
+                                    <td>{movieObj.genre.name}</td>
+                                    <td>{movieObj.numberInStock}</td>
+                                    <td>{movieObj.dailyRentalRate}</td>
+                                    <td><button type="button" className="btn btn-danger" onClick={() => { this.deleteEntry(movieObj._id); }}>Delete</button></td>
+
+                                </tr>
+
+
                                 )
-                        
-                            } )}
+
+                            })}
                         </tbody>
                     </table>
+                    <div className="row">
+                        
+                        {/* <input type="number" className="pageNumber" placeholder="page number" /> */}
+                        <input type="number" className="col-1"
+                            placeholder="no of elements/page"
+                            value={limit}
+                            onChange={this.changelimit}
+                        />
+                        <nav aria-label="..." className="col-2">
+                            <ul className="pagination ">
+                                <li className="page-item active">
+                                    <span className="page-link">1</span>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            </ul>
+                        </nav>
                 </div>
                 
-                
+                </div>
+
+
             </div>
         )
     }
